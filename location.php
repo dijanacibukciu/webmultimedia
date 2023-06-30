d32e0c1f3d6e5a76fc53e054e8f4bd30237c77d8
dijanacibukciu <93317396+dijanacibukciu@users.noreply.github.com>
2023-06-30 18:31:12 +0200

    Add files via upload

---
+++ location.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="CSS/location.css">
+
+    <style>
+        *{
+        margin: 0px;
+        padding: 0px;
+        }
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: grey;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover {
+            background-color: darkgrey;
+        }
+        .logo{
+            margin-left: 10px;
+        }
+        .footer{
+            background-color: lightgrey;
+        }
+        h6{
+            margin-left: 15px;
+            margin-top: 20px;
+            font-size: 15px;
+        }
+        .paradise{
+            margin-left: 15px;
+            margin-top: 8px;
+        }
+        #btn1{
+            margin-top: 20px;
+        }
+        #telephone{
+            margin-top: 8px;
+            margin-right: 15px;
+        }
+    </style>
+</head>
+<body>
+    <!--NAVBAR-->
+    <div class="navbar">
+        <ul>
+            <li><img src="Fotot/LOGO.png" alt="logo" style="width:100px; height: 40px" class="logo"></li>
+            <li><a href="book.php">Book</a></li>
+            <li><a href="photos.php">Photos</a></li>
+            <li><a href="location.php">Location</a></li>
+            <li><a href="activities.php">Activities</a></li>
+            <li><a href="contact.php">More</a></li>
+            <li><a href="login.php">Log in</a></li>
+            <!-- <li><a href="view/register.php">Register</a></li> -->
+        </ul>
+    </div>
+
+   <!--MAP-->
+    <div class="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4074151.566875115!2d71.3124053625!3d4.285395199999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b3f81597ff65abb%3A0x515849bd8413823e!2sParadise%20Island%20Resort%2C%20Maldives!5e0!3m2!1sen!2s!4v1579880458041!5m2!1sen!2s" 
+        width="100%" height="600"></iframe>
+    </div>
+
+    <!--FOOTER-->
+    <footer>
+       <hr>
+       <div class="footer">
+            <div style="float: left">
+                <h6>Find us at:</h6>
+                <p class="paradise">paradisehotel@gmail.com</p> 
+            </div>
+    
+        
+            <div style="float: right">
+                <button id="btn1"><img src="Fotot/telephone.png" width="30px" height="30px"></button>
+                <p id="telephone"> + 01 234 567 89</p>
+                <br>
+            </div>
+        </div>
+   </footer>
+
+</body>
+</html>
